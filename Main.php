aade84a3c36bd6592f628c6f25536d62116db83f
niekmussche <niekmussche@live.nl>
2017-03-22 16:19:32 +0100

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Niekert\Discord;
+
+use pocketmine\plugin\PluginBase;
+
+class Main extends PluginBase{
+	
+	public function onLoad(){
+		$this->getLogger()->info("Plugin loading");
+	}
+		
+	public function onEnable(){
+		$this->getLogger()->info("Plugin enabled");
+		$this->saveDefaultConfig();
+		$this->reloadConfig();
+		$username = $this->getConfig()->get("username");
+		$message = $this->getConfig()->get("message");
+		$webhook = $this->getConfig()->get("webhook_url");
+		$this->reloadConfig();
+		$data = array("content" => $message, "username" => "$username",);
+		$curl = curl_init("$webhook");
+		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
+		curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		return curl_exec($curl);
+	}
+}
