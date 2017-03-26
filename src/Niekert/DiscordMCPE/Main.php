5bf557ba6d5aee43a7ce5ea315f1e73ec3e402c0
niekmussche <niekmussche@live.nl>
2017-03-26 13:32:57 +0200

    Added Join event

--- src/Niekert/DiscordMCPE/Main.php
+++ src/Niekert/DiscordMCPE/Main.php
@@ -4,20 +4,27 @@ namespace Niekert\DiscordMCPE;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\event\Listener;
 
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 	
 	public function onLoad(){
 		$this->getLogger()->info("Plugin loading");
 	}
 		
 	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->getLogger()->info("Plugin enabled");
 		$this->saveDefaultConfig();
 		$this->reloadConfig();
 		$username = $this->getConfig()->get("username");
 		$message = $this->getConfig()->get("message");
 		$webhook = $this->getConfig()->get("webhook_url");
+		$playerjoinopt = $this->getConfig()->get("join_message");
                 
                 // Statements
                 
@@ -45,5 +52,25 @@ class Main extends PluginBase{
                     $this->getLogger()->warning('Check your Discord Server now :)');
                 
                 }
+	}
+
+	public function onJoin(PlayerJoinEvent $event){
+		if ($playerjoinopt === "0") {
+			$event->setCancelled()
+  	}
+		
+		else {
+			$name = $event->getPlayer();
+			$player = $player->getName();
+			$playerjoindata = array("content" => $playerjoinopt, "username" => "$username");
+  	    $curl = curl_init();
+						curl_setopt($curl, CURLOPT_URL, $webhook);
+						curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($playerjoindata));
+						curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+						curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+						curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+						curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+						curl_exec($curl);
+		}
 	}
 }
