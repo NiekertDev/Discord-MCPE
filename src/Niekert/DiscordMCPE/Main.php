7c84c93380660a2154406ff788d93fce256d05da
niekmussche <niekmussche@live.nl>
2017-04-12 16:59:44 +0200

    Make it work :smirk: and added colors.

--- src/Niekert/DiscordMCPE/Main.php
+++ src/Niekert/DiscordMCPE/Main.php
@@ -3,6 +3,7 @@
 namespace Niekert\DiscordMCPE;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
 use pocketmine\utils\Config;
 use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\player\PlayerQuitEvent;
@@ -10,6 +11,8 @@ use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\Player;
 use pocketmine\Server;
 use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
 
 class Main extends PluginBase implements Listener{
 
@@ -20,7 +23,7 @@ class Main extends PluginBase implements Listener{
 	public function onEnable(){
 		$this->getServer()->getPluginManager()->registerEvents($this,$this);
 		$this->saveDefaultConfig();
-		$this->getLogger()->info("Plugin enabled");
+		$this->getLogger()->info(TextFormat::GREEN."Plugin enabled");
 		$this->reloadConfig();
 		$this->webhook = $this->getConfig()->get("webhook_url");
 		$this->botusername = $this->getConfig()->get("username");
@@ -30,11 +33,12 @@ class Main extends PluginBase implements Listener{
 		$this->quitopt = $this->getConfig()->get("quit_message");
 		$this->deathopt = $this->getConfig()->get("death_message");
 		$this->debugopt = $this->getConfig()->get("debug");
+		$this->commandopt = $this->getConfig()->get("command");
 
 			//I'm to lazy to set a message for all options :)
 				
 				if($this->webhook === "" OR $this->botusername === "" OR $this->startupopt === "" OR $this->shutdownopt === "" OR $this->joinopt === "" OR $this->quitopt === "" OR $this->deathopt === ""){
-					$this->getLogger()->warning('Please edit your config.yml');
+					$this->getLogger()->warning(TextFormat::RED.'Please edit your config.yml');
 					$this->setEnabled(false);
 					return;
 				}
@@ -42,13 +46,13 @@ class Main extends PluginBase implements Listener{
 				elseif($this->startupopt !== "0"){
 					$this->send($this->startupopt, $this->botusername);
 						if($this->error === "0"){
-							$this->getLogger()->info('Check your Discord Server now :)');
+							$this->getLogger()->info(TextFormat::GREEN.'Check your Discord Server now :)');
 						}
 				}
 	}
 	
 	public function onDisable(){
-        $this->getLogger()->info("Plugin Disabled");
+        $this->getLogger()->info(TextFormat::RED."Plugin Disabled");
 		if($this->shutdownopt !== "0" AND $this->webhook !== "" AND $this->botusername !== "" AND $this->startupopt !== ""){
 			$this->send($this->shutdownopt, $this->botusername);
 		}
@@ -77,6 +81,30 @@ class Main extends PluginBase implements Listener{
 			$this->send(str_replace("{player}","$player","$this->deathopt"), $this->botusername);
 		}
 	}
+	
+	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
+		if($cmd->getName() == "discord"){
+			if($this->commandopt !== "0"){
+				if(!isset($args[0])) {
+					$sender->sendMessage(TextFormat::RED."Please provide an argument! Usage: /discord (message).");
+				}
+				else{
+					$sendername = $sender->getName();
+					$this->send($args[0], $sendername);
+					if($this->error === "0"){;
+						$sender->sendMessage(TextFormat::GREEN."Discord message was send.");
+					}
+					else{
+						$sender->sendMessage(TextFormat::RED."Discord message wasn't send.");
+					}
+				}
+			}
+			else{
+				$sender->sendMessage(TextFormat::RED."Sorry, but the owner disabled this option.");
+			}
+		}
+	return true;
+	}
 
 	function send($message, $username){
 		$data = array("content" => $message, "username" => "$username");
@@ -91,12 +119,12 @@ class Main extends PluginBase implements Listener{
 			$curlerror = curl_error($curl);
 			
 				if($response === false AND $this->debugopt === "1"){
-					$this->getLogger()->warning('ERROR: ' .$curlerror);
+					$this->getLogger()->error(TextFormat::RED.'ERROR: ' .$curlerror);
 					$error = "1";
 				}
 				
 				elseif($response === false AND $this->debugopt === "0"){
-					$this->getLogger()->warning('Something strange happened :(. Set the debug option in the config to 1 to show the error.');
+					$this->getLogger()->warning(TextFormat::RED.'Something strange happened :(. Set the debug option in the config to 1 to show the error.');
 					$error = "1";
 				}
 				
