f7201d588a0730e71cef385ebc3d93bf53e4b838
niekmussche <niekmussche@live.nl>
2017-06-15 22:49:42 +0200

    ADDED ASYNC BITCHES

--- src/Niekert/DiscordMCPE/Main.php
+++ src/Niekert/DiscordMCPE/Main.php
@@ -2,113 +2,65 @@
 
 namespace Niekert\DiscordMCPE;
 
+use pocketmine\command\ConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\TextFormat;
-use pocketmine\utils\Config;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerDeathEvent;
-use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\Player;
-use pocketmine\Server;
+use pocketmine\utils\TextFormat as C;
 use pocketmine\event\Listener;
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use Niekert\DiscordMCPE\Events\EventListener;
 
 class Main extends PluginBase implements Listener{
 	
-	public $debug_opt;
+	public $webhook, $username, $startupopt, $shutdownopt, $joinopt, $quitopt, $deathopt, $debugopt, $commandopt, $chaturl, $chatformat, $chatprefix, $chatopt, $chatuser;
 	
 	public function onLoad(){
 		$this->getLogger()->info("Plugin loading");
 	}
 		
 	public function onEnable(){
-		$this->getServer()->getPluginManager()->registerEvents($this,$this);
-		$this->saveDefaultConfig();
-		$this->getLogger()->info(TextFormat::GREEN."Plugin enabled");
-		$this->setvars();
-		
-				if($this->startupopt !== "0" AND $this->webhook !== "" AND $this->botusername !== "" AND $this->startupopt !== ""){
-					$response = $this->send($this->startupopt, $this->botusername);
-						if($response){
-							$this->getLogger()->info(TextFormat::GREEN.'Check your Discord Server now :)');
-						}
-				}
+        $this->getServer()->getScheduler()->scheduleTask(new Utils\ConfigUpdater($this));
+        foreach ($this->getConfig()->getAll() as $item){
+            if(!isset($item) OR ""){
+                $this->getLogger()->info("Please edit your config");
+                $this->setEnabled(false);
+            }
+        }
+        $this->setvars();
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+		$this->getLogger()->info(C::GREEN."Plugin enabled");
+        if($this->getConfig()->get("start_message") !== "0"){
+            $player = new ConsoleCommandSender();
+            $this->sendMessage($this->webhook, $this->startupopt, $player);
+        }
 	}
 	
 	public function onDisable(){
-        $this->getLogger()->info(TextFormat::RED."Plugin Disabled");
-		if($this->shutdownopt !== "0" AND $this->webhook !== "" AND $this->botusername !== "" AND $this->startupopt !== ""){
-			$this->send($this->shutdownopt, $this->botusername);
+        $this->getLogger()->info(C::RED."Plugin Disabled");
+		if($this->shutdownopt !== "0" AND !$this->isEnabled()){
+			$this->sendMessage($this->webhook, $this->shutdownopt);
 		}
     }
 
-	public function onJoin(PlayerJoinEvent $event){
-		$temp1 = $event->getPlayer();
-		$player = $temp1->getName();
-		if($this->joinopt !== "0"){
-			$this->send(str_replace("{player}","$player","$this->joinopt"), $this->botusername);
-		}
-	}
-
-	public function onQuit(PlayerQuitEvent $event){
-		$temp2 = $event->getPlayer();
-		$player = $temp2->getName();
-		if($this->quitopt !== "0"){
-			$this->send(str_replace("{player}","$player","$this->quitopt"), $this->botusername);
-		}
-	}	
-
-	public function onDeath(PlayerDeathEvent $event){
-		$temp3 = $event->getEntity();
-		$player = $temp3->getName();
-		if($this->joinopt !== "0"){
-			$this->send(str_replace("{player}","$player","$this->deathopt"), $this->botusername);
-		}
-	}
-	
-	public function onChat(PlayerChatEvent $event){
-		$message = $event->getMessage();
-		$sender = $event->getPlayer();
-		if($this->chatprefix !== "0"){
-			$format = str_replace(array('{player}', '{message}'), array($sender->getName(), ltrim($message, $this->chatprefix)), $this->chatformat);
-			if(substr($message, 0, 1 ) === $this->chatprefix){
-				$event->setCancelled(true);
-				$response = $this->send($format, $this->chatuser, $this->chaturl);
-				if($response){
-					$sender->sendMessage(TextFormat::GREEN."Discord message was send.");
-				}
-				elseif(!$response){
-					$sender->sendMessage(TextFormat::RED."Discord message wasn't send.");
-				}
-			}
-		}
-		if($this->chatopt !== "0"){
-			$format = str_replace(array('{player}', '{message}'), array($sender->getName(), $message), $this->chatformat);
-			$this->send($format, $this->chatuser, $this->chaturl);
-		}
-	}
-	
 	public function onCommand(CommandSender $sender, Command $cmd, $label, array $args){
 		if($cmd->getName() == "discord"){
 			if($this->commandopt !== "0"){
 				if(!isset($args[0])) {
-					$sender->sendMessage(TextFormat::RED."Please provide an argument! Usage: /discord (message).");
+					$sender->sendMessage(C::RED."Please provide an argument! Usage: /discord (message).");
 				}
 				else{
 					$format = str_replace(array('{player}', '{message}'), array($sender->getName(), implode(" ", $args)), $this->chatformat);
-					$response = $this->send($format, $this->chatuser, $this->chaturl);
+					$response = $this->sendMessage($this->chaturl, $format, $sender, $this->chatuser);
 					if($response){
-						$sender->sendMessage(TextFormat::GREEN."Discord message was sent.");
+						$sender->sendMessage(C::GREEN."Discord message was sent.");
 					}
 					elseif(!$response){
-						$sender->sendMessage(TextFormat::RED."Discord message wasn't sent.");
+						$sender->sendMessage(C::RED."Discord message wasn't sent.");
 					}
 				}
 			}
 			else{
-				$sender->sendMessage(TextFormat::RED."Sorry, but the owner disabled this option.");
+				$sender->sendMessage(C::RED."Sorry, but the owner disabled this option.");
 			}
 		}
 	return true;
@@ -117,7 +69,7 @@ class Main extends PluginBase implements Listener{
 	function setvars(){
 		$this->reloadConfig();
 		$this->webhook = $this->getConfig()->get("webhook_url");
-		$this->botusername = $this->getConfig()->get("username");
+		$this->username = $this->getConfig()->get("username");
 		$this->startupopt = $this->getConfig()->get("start_message");
 		$this->shutdownopt = $this->getConfig()->get("shutdown_message");
 		$this->joinopt = $this->getConfig()->get("join_message");
@@ -125,20 +77,13 @@ class Main extends PluginBase implements Listener{
 		$this->deathopt = $this->getConfig()->get("death_message");
 		$this->debugopt = $this->getConfig()->get("debug");
 		$this->commandopt = $this->getConfig()->get("command");
-		$this->chaturlopt = $this->getConfig()->get("chat_url");
 		$this->chatformat = $this->getConfig()->get("chat_format");
 		$this->chatprefix = $this->getConfig()->get("chat_prefix");
 		$this->chatopt = $this->getConfig()->get("chat");
 		
 			//Some statements
-			//I'm to lazy to set a message for all options :)
-			if($this->webhook === "" OR $this->botusername === "" OR $this->startupopt === "" OR $this->shutdownopt === "" OR $this->joinopt === "" OR $this->quitopt === "" OR $this->deathopt === "" OR $this->getConfig()->get("chat_url") === "" OR $this->getConfig()->get("chat_username") === ""){
-				$this->getLogger()->warning(TextFormat::RED.'Please edit your config.yml');
-				$this->setEnabled(false);
-				return;
-			}
 			if($this->getConfig()->get("chat_username") === "0"){
-				$this->chatuser = $this->botusername;
+				$this->chatuser = $this->username;
 			}
 			elseif($this->getConfig()->get("chat_username") !== "0"){
 				$this->chatuser = $this->getConfig()->get("chat_username");
@@ -151,25 +96,35 @@ class Main extends PluginBase implements Listener{
 			}
 	}
 
-	public function error($error){
-			$this->getLogger()->error(TextFormat::RED.$error);
-	}
-	
-	function send($message, $username, $webhook = ""){
-		if($webhook === ""){
-			$webhook = $this->webhook;
-		}
-		$task = new SendMessage($this, $message, $username, $webhook);
-		$this->getServer()->getScheduler()->scheduleAsyncTask($task);
-		if($task){
-			return true;
-		}
-		elseif(!$task){
-			return false;
-		}
-		//For testing
-		else{
-			$this->getLogger()->error(TextFormat::RED.'oops');
-		}
-	}
+	public function notify($player, $result){
+        if($player === "console"){ //If no player is specified
+            return;
+        }
+        else{
+            if($result["success"]) {
+                $player->sendMessage(C::AQUA."[Discord-MCPE] ".C::GREEN."Discord message was send!");
+                return;
+            }
+            else{
+                if($this->getConfig()->get("debug")){
+                    $this->getLogger()->error(C::RED."Error: ".$result["Error"]);
+                }
+                else{
+                    $this->getLogger()->warning(C::RED."Something strange happened. Set debug in config to true to get error message");
+                }
+                $player->sendMessage(C::AQUA."[Discord-MCPE] ".C::GREEN."Discord message wasn't send!");
+            }
+        }
+    }
+
+    public function sendMessage($webhook, $message, $player = "console", $username = null){
+	    if(!isset($username)){
+	        $username = $this->username;
+        }
+	    $curlopts = [
+	        "content" => $message,
+            "username" => $username
+        ];
+        $this->getServer()->getScheduler()->scheduleAsyncTask(new Tasks\SendTaskAsync($player, $webhook, $curlopts));
+    }
 }
